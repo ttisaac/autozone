9d18e8c499106eb1af8731f86a6847d51903a1b7
Isaac <ttorresisaac@gmail.com>
2023-05-11 18:57:02 -0600

    Remove unnecessary tags from sidebar

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -12,24 +12,12 @@ const Sidebar = () => {
           <h5 className='italic text-white' >Metric Threshold Calculator</h5>
         </div>
         
-        <div className='w-full'>
-          <ul className='flex flex-col gap-5'>
-            <li>
-              <SidebarItem title={'Metrics List'} destination='/.' imgsrc='/images/stats.png' alt='Graph' />
-            </li>
-            <li>
-              <SidebarItem title={'Recalculation'} destination='/.' imgsrc='/images/recalculate.png' alt='Graph' />
-            </li>
-            <li>
-              <SidebarItem title={'YAML Status'} destination='/.' imgsrc='/images/file-export.png' alt='Graph' />
-            </li>
-            <li>
-              <SidebarItem title={'Configure YAML File'} destination='/.' imgsrc='/images/edit-yaml.png' alt='Graph' />
-            </li>
-            <li>
-              <SidebarItem title={'Settings'} destination='/.' imgsrc='/images/settings.png' alt='Graph' />
-            </li>
-          </ul>
+        <div className='w-full flex flex-col gap-5'>
+            <SidebarItem title={'Metrics List'} destination='/' imgsrc='/images/stats.png' alt='Graph' />
+            <SidebarItem title={'Recalculation'} destination='/' imgsrc='/images/recalculate.png' alt='Graph' />
+            <SidebarItem title={'YAML Status'} destination='/' imgsrc='/images/file-export.png' alt='Graph' />
+            <SidebarItem title={'Configure YAML File'} destination='/.' imgsrc='/images/edit-yaml.png' alt='Graph' />
+            <SidebarItem title={'Settings'} destination='/' imgsrc='/images/settings.png' alt='Graph' />
         </div>
     </div>
   )
