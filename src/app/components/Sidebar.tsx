12b2e4122fb53229c8f4ddf5fb27c5e4c6a44d95
Isaac <ttorresisaac@gmail.com>
2023-05-12 23:40:58 -0600

    Refactor code with a map for the items

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -3,6 +3,41 @@ import SidebarItem from './SidebarItem'
 import Image from 'next/image'
 
 const Sidebar = () => {
+
+  const pages = [
+    {
+      title: 'Metrics List',
+      path: '/',
+      imgsrc: '/images/stats.png',
+      alt: 'A bar graph icon'
+    },
+    {
+      title: 'Recalculation',
+      path: '/Recalculation',
+      imgsrc: '/images/recalculate.png',
+      alt: 'A circular arrow icon'
+    },
+    {
+      title: 'YAML Status',
+      path: '/YamlStatus',
+      imgsrc: '/images/file-export.png',
+      alt: 'Document icon with a sideways arrow'
+    },
+    {
+      title: 'Configure YAML File',
+      path: '/ConfigureFile',
+      imgsrc: '/images/edit-yaml.png',
+      alt: 'Icon for document editing with a pen'
+    },
+    {
+      title: 'Settings',
+      path: '/Settings',
+      imgsrc: '/images/settings.png',
+      alt: 'A settings icon featuring a gear symbol',
+      lastItem: true
+    }
+  ];
+
   return (
     <div className='h-screen w-72 bg-sidebar pt-4
                     flex flex-col items-center'>
@@ -13,11 +48,16 @@ const Sidebar = () => {
         </div>
         
         <div className='w-full flex flex-col'>
-            <SidebarItem title={'Metrics List'} destination='/' imgsrc='/images/stats.png' alt='Graph' />
-            <SidebarItem title={'Recalculation'} destination='/Recalculation' imgsrc='/images/recalculate.png' alt='Graph' />
-            <SidebarItem title={'YAML Status'} destination='/YamlStatus' imgsrc='/images/file-export.png' alt='Graph' />
-            <SidebarItem title={'Configure YAML File'} destination='/ConfigureFile' imgsrc='/images/edit-yaml.png' alt='Graph' />
-            <SidebarItem title={'Settings'} destination='/Settings' imgsrc='/images/settings.png' alt='Graph' lastItem />
+        {pages.map((page, index) => (
+        <SidebarItem
+          key={index}
+          title={page.title}
+          destination={page.path}
+          imgsrc={page.imgsrc}
+          alt={page.alt}
+          lastItem={page.lastItem}
+        />
+        ))}
         </div>
 
         <div className='mt-auto mb-4 text-sidebar-text'>©All rights reserved</div>
