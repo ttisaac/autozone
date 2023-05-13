6d44b558ffe9597729838c019e5a2a868ef6f073
Isaac <ttorresisaac@gmail.com>
2023-05-13 12:20:42 -0600

    Improve table design
    Improve accessibility of the table by adding scopes, and also added
    colgroups for repeated styles.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,16 +20,16 @@ export default function HomePage() {
           <table className='ml-auto w-80 border-2 border-black'>
             <thead>
               <tr>
-                <th className='p-2 text-2xl border-b-2 border-black'>Week</th>
+                <th scope="col" className='p-2 text-2xl border-b-2 border-black'>Week</th>
               </tr>
             </thead>
+
             <tbody className='h-40 overflow-y-scroll block pt-2'>
-              {/* Agregar despues un map para los valores */}
               <tr className='block pl-4 pb-2 mb-2 border-b-2 border-zinc-300'>
-                  <td className='flex items-center'>
-                   <input type='checkbox' className='w-7 h-7' />
-                   <label className='ml-4 text-xl'>Week1 20223</label>
-                  </td>
+                <td className='flex items-center'>
+                  <input type='checkbox' className='w-7 h-7' />
+                  <label className='ml-4 text-xl'>Week1 20223</label>
+                </td>
               </tr>
             </tbody>
           </table>
@@ -37,26 +37,34 @@ export default function HomePage() {
 
         <div className="pt-4">
           <table className='week-table w-full border-2 border-white'>
+            <colgroup>
+              <col className='w-3/12 border-2 border-white'/>
+              <col className='w-5/12 border-2 border-white'/>
+              <col className='w-1/12 border-2 border-white'/>
+              <col className='w-3/12 border-2 border-white'/>
+            </colgroup>
+
             <thead className=' bg-zinc-400'>
               <tr className='text-white'>
-                <th className='w-3/12 border-2 border-white text-left p-3 text-xl'>Week in which it was calculated</th>
-                <th className='w-5/12 border-2 border-white text-left p-3 text-xl'>Date range</th>
-                <th className='w-1/12 border-2 border-white text-left p-3 text-xl'>Official</th>
-                <th className='w-3/12 border-2 border-white text-left p-3 text-xl'> </th>
+                <th scope="col" className='text-left p-3 text-xl'>Week in which it was calculated</th>
+                <th scope="col" className='text-left p-3 text-xl'>Date range</th>
+                <th scope="col" className='text-left p-3 text-xl'>Official</th>
+                <th scope="col" className='text-left p-3 text-xl'>&nbsp;</th>
               </tr>
             </thead>
+
             <tbody className='border-t-8 border-white'>
               <tr>
-                <td className='w-3/12 border-2 border-white text-left p-3 text-xl'>Week 1</td>
-                <td className='w-5/12 border-2 border-white text-left p-3 text-xl'>January 1-Sunday, January 7, 2023</td>
-                <td className='w-1/12 border-2 border-white text-center p-3 text-xl font-bold underline'>False</td>
-                <td className='w-3/12 border-2 border-white'></td>
+                <td className='text-left p-3 text-xl'>Week 1</td>
+                <td className='text-left p-3 text-xl'>January 1-Sunday, January 7, 2023</td>
+                <td className='text-center p-3 text-xl font-bold underline'>False</td>
+                <td className='text-red-500 text-xl text-center underline font-bold'>Recalculation</td>
               </tr>
               <tr>
-                <td className='w-3/12 border-2 border-white text-left p-3 text-xl'>Week 1</td>
-                <td className='w-5/12 border-2 border-white text-left p-3 text-xl'>January 1-Sunday, January 7, 2023</td>
-                <td className='w-1/12 border-2 border-white text-center p-3 text-xl font-bold underline'>False</td>
-                <td className='w-3/12 border-2 border-white'></td>
+                <td className='text-left p-3 text-xl'>Week 1</td>
+                <td className='text-left p-3 text-xl'>January 1-Sunday, January 7, 2023</td>
+                <td className='text-center p-3 text-xl font-bold underline'>False</td>
+                <td className='text-red-500 text-xl text-center underline font-bold'>&nbsp;</td>
               </tr>
             </tbody>
           </table>
